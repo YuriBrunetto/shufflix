ca5b53906ec617a62522540bb94f28c905e3e16a
Yuri Brunetto <brunettoyuri@gmail.com>
2021-03-04 18:31:29 -0300

    seo stuff

--- components/seo/index.js
+++ components/seo/index.js
@@ -1,48 +1,47 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Head from 'next/head'
 
 const SEO = () => (
   <Head>
-    <title>{'Shufflix'}</title>
+    <title>Shufflix</title>
     <link rel='icson' href='/favicon.ico' />
     <link rel='manifest' href='/manifest.json' />
     <meta name='theme-color' content='#706FD3' />
     <meta name='title' content='Shufflix' />
     <meta
       name='description'
-      content='Shuffle an episode of your favorte Show'
+      content='Shuffle an episode of your favorite Show'
     />
 
     <meta property='og:type' content='website' />
-    {/* <meta
-      property='og:url'
-      content={url ? url : 'https://yuribrunetto.com.br/'}
-    /> */}
+    <meta property='og:url' content='https://shufflix.vercel.app/' />
     <meta property='og:title' content='Shufflix' />
     <meta
       property='og:description'
-      content='Shuffle an episode of your favorte Show'
+      content='Shuffle an episode of your favorite Show'
     />
-    {/* <meta
+    <meta
       property='og:image'
-      content='https://yuribrunetto.com.br/logo512.png'
-    /> */}
+      content='https://shufflix.vercel.app/logo512.png'
+    />
 
-    {/* <meta property='twitter:card' content='summary_large_image' /> */}
-    {/* <meta
-      property='twitter:url'
-      content={url ? url : 'https://yuribrunetto.com.br/'}
-    /> */}
-    <meta property='twitter:title' content={'Shufflix'} />
+    <link
+      rel='apple-touch-icon'
+      href='https://shufflix.vercel.app/logo512.png'
+    />
+    <meta name='apple-mobile-web-app-title' content='Shufflix' />
+
+    <meta property='twitter:card' content='summary_large_image' />
+    <meta property='twitter:url' content='https://shufflix.vercel.app/' />
+    <meta property='twitter:title' content='Shufflix' />
     <meta
       property='twitter:description'
-      content='Shuffle an episode of your favorte Show'
+      content='Shuffle an episode of your favorite Show'
     />
-    {/* <meta
+    <meta
       property='twitter:image'
-      content='https://yuribrunetto.com.br/twitter-card.png'
-    /> */}
+      content='https://shufflix.vercel.app/twitter-card.png'
+    />
   </Head>
 )
 
